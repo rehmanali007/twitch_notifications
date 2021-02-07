390e5892e7435e9c8cd567bb294ac91e8e72dae0
Rehman Ali <rehmanali.9442289@gmail.com>
2021-02-07 20:20:53 +0500

    Successful authentications

--- main.py
+++ main.py
@@ -1,64 +1,100 @@
-from twitchAPI.oauth import UserAuthenticator
+from twitchAPI import UserAuthenticator
 from twitchAPI.twitch import Twitch
-from twitchAPI.webhook import TwitchWebHook
 from twitchAPI.types import AuthScope
+from twitchAPI.webhook import TwitchWebHook
 from pprint import pprint
 import json
 import ssl
 
 config = json.load(open('config.json', 'r'))
+IP_ADDRESS = config.get('PUBLIC_ADDR')
+PORT = config.get('PUBLIC_PORT')
+APP_ID = config.get('APP_ID')
+APP_SECRET = config.get('APP_SECRET')
+PUBLIC_ADDR = f'https://{IP_ADDRESS}:{PORT}'
+
+TOKEN = config.get("TOKEN")
+REFRESH_TOKEN = config.get("REFRESH_TOKEN")
 
 
 def callback_stream_changed(uuid, data):
-    print('Callback for UUID ' + str(uuid))
+    print('Callback Stream changed for UUID ' + str(uuid))
     pprint(data)
 
 
-app_id = config.get('APP_ID')
-app_secret = config.get('APP_SECRET')
-public_addr = config.get('PUBLIC_ADDR')
-public_port = config.get('PUBLIC_PORT')
+def callback_user_changed(uuid, data):
+    print('Callback User changed for UUID ' + str(uuid))
+    pprint(data)
 
-my_public_url = f'https://{public_addr}:{public_port}'
 
-twitch = Twitch(app_id, app_secret)
-authentication_scope = [
+# basic twitch API authentication, this will yield a app token but not a user token
+auth_scope = [
     AuthScope.BITS_READ,
     AuthScope.USER_EDIT,
+    AuthScope.WHISPERS_READ,
+    AuthScope.CHANNEL_READ_SUBSCRIPTIONS,
+    AuthScope.CHANNEL_READ_STREAM_KEY,
+    AuthScope.ANALYTICS_READ_EXTENSION,
+    AuthScope.ANALYTICS_READ_GAMES,
+    AuthScope.CHANNEL_EDIT_COMMERCIAL,
+    AuthScope.CHANNEL_READ_HYPE_TRAIN,
+    AuthScope.CHANNEL_MANAGE_BROADCAST,
+    AuthScope.CHANNEL_READ_REDEMPTIONS,
+    AuthScope.CLIPS_EDIT,
+    AuthScope.USER_EDIT_BROADCAST,
     AuthScope.USER_READ_BROADCAST,
-    AuthScope.USER_READ_EMAIL
+    AuthScope.USER_READ_EMAIL,
+    AuthScope.USER_EDIT_FOLLOWS,
+    AuthScope.CHANNEL_MODERATE,
+    AuthScope.CHAT_EDIT,
+    AuthScope.CHAT_READ,
+    AuthScope.WHISPERS_READ,
+    AuthScope.WHISPERS_EDIT,
+    AuthScope.MODERATION_READ,
+    AuthScope.CHANNEL_SUBSCRIPTIONS
 ]
-twitch.authenticate_app(authentication_scope)
-
-twitch = Twitch(app_id, app_secret)
-
-target_scope = [AuthScope.BITS_READ]
-auth = UserAuthenticator(twitch, target_scope, force_verify=True)
-# this will open your default browser and prompt you with the twitch verification website
-token, refresh_token = auth.authenticate()
-print(token, refresh_token)
-# add User authentication
-twitch.set_user_authentication(token, target_scope, refresh_token)
-
-
-my_user_name = 'rehmanali1337'
-user_info = twitch.get_users(my_user_name)
-print(user_info)
-user_id = user_info['data'][0]['id']
-# basic setup
-
+twitch = Twitch(APP_ID, APP_SECRET)
+twitch.authenticate_app(auth_scope)
+# since we want user information, we require a OAuth token, lets get one
+# you dont need to generate a fresh user token every time, you can also refresh a old one or get one using a different online service
+# for refreshing look here: https://github.com/Teekeks/pyTwitchAPI#user-authentication
+# please note that you have to add http://localhost:17563 as a OAuth redirect URL for your app, see the above link for more information
+# auth = UserAuthenticator(
+#     twitch, auth_scope)
+# token, refresh_token = auth.authenticate()  # this will open a webpage
+# print(token, refresh_token)
+# setting the user authentication so any api call will also use it
+twitch.set_user_authentication(
+    TOKEN, auth_scope, REFRESH_TOKEN)
+# setting up the Webhook itself
 sslContext = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS)
-hook = TwitchWebHook(my_public_url, app_id, public_port)
+hook = TwitchWebHook(PUBLIC_ADDR,
+                     APP_ID, PORT, ssl_context=sslContext)
+# this will use the highest authentication set, which is the user authentication.
 hook.authenticate(twitch)
+# some hooks don't require any authentication, which would remove the requirement to set up a https reverse proxy
+# if you don't require authentication just dont call authenticate()
 hook.start()
 
-print('[+] Subscribing to hook ...')
-success, uuid = hook.subscribe_stream_changed(user_id, callback_stream_changed)
-if not success:
-    print('Could not subscribe to webhook ...')
-    hook.stop()
-pprint(twitch.get_webhook_subscriptions())
-# the webhook is now running and you are subscribed to the topic you want to listen to. lets idle a bit...
-input('[+] Press "Enter" to shut down...\n')
+user_info = twitch.get_users()
+user_id = user_info['data'][0]['id']
+print(f'User ID : {user_id}')
+
+
+# the hook has to run before you subscribe to any events since the twitch api will do a handshake this this webhook as soon as you subscribe
+success, uuid_stream = hook.subscribe_stream_changed(
+    user_id, callback_stream_changed)
+print(uuid_stream)
+print(f'Was subscription successfull?: {success}')
+success, uuid_user = hook.subscribe_user_changed(
+    user_id, callback_user_changed)
+print(f'Was subscription successfull?: {success}')
+
+# now we are fully set up and listening to our webhooks, lets wait for a user imput to stop again:
+input('[+] Press enter to stop...')
+
+# lets unsubscribe again
+success = hook.unsubscribe(uuid_user)
+print(f'was unsubscription successfull?: {success}')
+# since hook.unsubscribe_on_stop is true, we dont need to unsubscribe manually, so lets just stop
 hook.stop()
-print('[+] Done!')
